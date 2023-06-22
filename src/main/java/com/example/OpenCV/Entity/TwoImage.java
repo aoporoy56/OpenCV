e384ed6a1ea0211227f634f5ba61fcdbbcb97d6c
aoporoy <aopo56@gmail.com>
2023-06-22 23:13:32 +0600

    Work Done

---
+++ src/main/java/com/example/OpenCV/Entity/TwoImage.java
@@ -0,0 +1,15 @@
+package com.example.OpenCV.Entity;
+
+import lombok.*;
+
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TwoImage {
+    private String image;
+    private String image2;
+
+}
